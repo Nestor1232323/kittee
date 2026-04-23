752e939ce983cc833a89bfc1da70efcb028e1edd
Нестор <116899923+Nestor1232323@users.noreply.github.com>
2026-04-23 13:32:39 +0300

    VERCEL CANNOT GIVE ME A FUCKING FILE FROM LOCALHOST!!!

--- api/v1/kittee/videos/getserverurl.js
+++ api/v1/kittee/videos/getserverurl.js
@@ -1,8 +1,23 @@
 export default function handler(request, response) {
+  // Устанавливаем заголовки CORS
+  response.setHeader('Access-Control-Allow-Credentials', true);
+  response.setHeader('Access-Control-Allow-Origin', '*'); // Разрешить всем
+  response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  response.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  // Обработка предварительного запроса (Preflight)
+  if (request.method === 'OPTIONS') {
+    response.status(200).end();
+    return;
+  }
+
   const data = {
     "status": "ok",
     "url": "d9f144df278c8c.lhr.life"
   };
 
-  response.status(200).json(data);
+  return response.status(200).json(data);
 }
